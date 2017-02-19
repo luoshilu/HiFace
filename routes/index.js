a323a7afddea65a3a5382b80c47f0e965f6ef555
luoshilu <978337784@qq.com>
2017-02-19 23:48:39 +0800

    "v1.0"

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.post('/first', function(req, res, next) {
+  res.json({name:'aaa',pwd:'123'});
+});
+module.exports = router;
